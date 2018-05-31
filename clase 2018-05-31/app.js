05102ae839673d9c7606c2bd9f7dad1b76c0c7f6
LeandroR18 <leandro.romagnoli15@gmail.com>
2018-05-31 12:07:19 -0300

    Press button change color

--- clase 2018-05-31/app.js
+++ clase 2018-05-31/app.js
@@ -1,1 +1,16 @@
 console.log("hellow");
+var changeColor = function(li) {
+  li.style.backgroundColor = "#f00"
+}
+
+window.onload = function () {
+
+  var btnPlay = document.getElementById('play');
+  btnPlay.onclick = function() {
+    console.log("click");
+    var liAlives = document.getElementsByClassName('alive');
+    for (var i = 0; i < liAlives.length; i++) {
+      changeColor(liAlives[i]);
+    }
+  }
+}
