0b3265ee905383dc22f4530fdd9f27a06f3cde35
LeandroR18 <leandro.romagnoli15@gmail.com>
2018-06-07 11:43:58 -0300

    Dinamic Cells

--- clase 2018-06-07/js/app.js
+++ clase 2018-06-07/js/app.js
@@ -1,12 +1,13 @@
-console.log("opr");
+console.log("ppr");
 
 var Play = function () {
-  var cells = Board.getCells();
-  Board.updateCells(cells)
+var cells = Board.getCells();
+Board.updateCells(cells)
 }
 
 window.onload = function () {
 
+  Board.createCells();
   var btnPlay = Board.getPlayButtton();
   btnPlay.onclick = play;
 }
